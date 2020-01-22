ae4afe717ed50434c89a4b0ee112afa4d5189675
Andre Costa <mr.luizandre@gmail.com>
2020-01-22 10:32:14 -0300

    Devise and Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :settings
-  resources :gallery_pictures
-  resources :galleries
+  devise_for :users, path_prefix: 'd'
   mount Ckeditor::Engine => '/ckeditor'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   scope path_names: { new: "novo", edit: "editar" } do
+    resources :users, path: 'usuarios'
+    resources :settings, path: 'ajustes'
+    resources :gallery_pictures, path: 'itens_da_galeria'
+    resources :galleries, path: 'galerias'
     resources :posts, path: 'noticias'
     resources :banners
     resources :pages, path: 'paginas'
